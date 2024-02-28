9f8121ec117ac872b76fc061fb1bc7d8e57fd555
AlexHoy915 <alex.hoy@jacks.sdstate.edu>
2024-02-28 08:13:45 -0600

    Slider touch-ups

--- arm_control/hello_world.py
+++ arm_control/hello_world.py
@@ -1,63 +1,63 @@
 import xarm
 
-#
+def hello_world():
 
     # arm is the first xArm detected which is connected to USB
-arm = xarm.Controller('USB')
-print('Battery voltage in volts:', arm.getBatteryVoltage())
+    arm = xarm.Controller('USB')
+    print('Battery voltage in volts:', arm.getBatteryVoltage())
 
-servo1 = xarm.Servo(1)
-servo2 = xarm.Servo(2)  
-servo3 = xarm.Servo(3) 
-servo4 = xarm.Servo(4) 
-servo5 = xarm.Servo(5) 
-servo6 = xarm.Servo(6) 
+    servo1 = xarm.Servo(1)
+    servo2 = xarm.Servo(2)  
+    servo3 = xarm.Servo(3) 
+    servo4 = xarm.Servo(4) 
+    servo5 = xarm.Servo(5) 
+    servo6 = xarm.Servo(6) 
 
-data = arm.getPosition(servo1, True)
-print("servo 1: ",data)
-data = arm.getPosition(servo2, True)
-print("servo 2: ",data)
-data = arm.getPosition(servo3, True)
-print("servo 3: ",data)
-data = arm.getPosition(servo4, True)
-print("servo 4: ",data)
-data = arm.getPosition(servo5, True)
-print("servo 5: ",data)
-data = arm.getPosition(servo6, True)
-print("servo 6: ",data)
+    data = arm.getPosition(servo1, True)
+    print("servo 1: ",data)
+    data = arm.getPosition(servo2, True)
+    print("servo 2: ",data)
+    data = arm.getPosition(servo3, True)
+    print("servo 3: ",data)
+    data = arm.getPosition(servo4, True)
+    print("servo 4: ",data)
+    data = arm.getPosition(servo5, True)
+    print("servo 5: ",data)
+    data = arm.getPosition(servo6, True)
+    print("servo 6: ",data)
 
-    # #Vertical Default
-    #arm.setPosition([[1, 0.0], [2, 0.0], [3, -90.0], [4, 0.0], [5, 0.0], [6, 0.0]], wait = True)
+    #Vertical Default
+    arm.setPosition([[1, 0.0], [2, 0.0], [3, -90.0], [4, 0.0], [5, 0.0], [6, 0.0]], wait = True)
 
-    # #South Down
-    # arm.setPosition([[1, 0.0], [2, 0.0], [3, 0.0], [4, -75.0], [5, 10.0], [6, 0.0]], wait=True)
+    #South Down
+    arm.setPosition([[1, 0.0], [2, 0.0], [3, 0.0], [4, -75.0], [5, 10.0], [6, 0.0]], wait=True)
 
-    # #South West
-    # arm.setPosition([[1, 0.0], [2, 0.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, -45.0]], wait=True)
+    #South West
+    arm.setPosition([[1, 0.0], [2, 0.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, -45.0]], wait=True)
 
-    # #Rotate Claw
-    # arm.setPosition([[1, 0.0], [2, 90.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, -45.0]], wait=True)
-    # arm.setPosition([[1, 0.0], [2, -90.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, -45.0]], wait=True)
-    # arm.setPosition([[1, 0.0], [2, 0.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, -45.0]], wait=True)
+    #Rotate Claw
+    arm.setPosition([[1, 0.0], [2, 90.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, -45.0]], wait=True)
+    arm.setPosition([[1, 0.0], [2, -90.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, -45.0]], wait=True)
+    arm.setPosition([[1, 0.0], [2, 0.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, -45.0]], wait=True)
 
-    # #Open Claw
-    # arm.setPosition([[1, 90.0], [2, 0.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, -45.0]], wait=True)
-    # arm.setPosition([[1, -90.0], [2, 0.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, -45.0]], wait=True)
+    #Open Claw
+    arm.setPosition([[1, 90.0], [2, 0.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, -45.0]], wait=True)
+    arm.setPosition([[1, -90.0], [2, 0.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, -45.0]], wait=True)
 
-    # #South East
-    # arm.setPosition([[1, 0.0], [2, 0.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, 45.0]], wait=True)
+    #South East
+    arm.setPosition([[1, 0.0], [2, 0.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, 45.0]], wait=True)
 
-    # #Rotate Claw
-    # arm.setPosition([[1, 0.0], [2, 90.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, 45.0]], wait=True)
-    # arm.setPosition([[1, 0.0], [2, -90.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, 45.0]], wait=True)
-    # arm.setPosition([[1, 0.0], [2, 0.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, 45.0]], wait=True)
+    #Rotate Claw
+    arm.setPosition([[1, 0.0], [2, 90.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, 45.0]], wait=True)
+    arm.setPosition([[1, 0.0], [2, -90.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, 45.0]], wait=True)
+    arm.setPosition([[1, 0.0], [2, 0.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, 45.0]], wait=True)
 
-    # #Open Claw
-    # arm.setPosition([[1, 90.0], [2, 0.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, 45.0]], wait=True)
-    # arm.setPosition([[1, -90.0], [2, 0.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, 45.0]], wait=True)
+    #Open Claw
+    arm.setPosition([[1, 90.0], [2, 0.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, 45.0]], wait=True)
+    arm.setPosition([[1, -90.0], [2, 0.0], [3, -45.0], [4, -75.0], [5, 10.0], [6, 45.0]], wait=True)
 
-    # #Vertical Default Setback
-    # arm.setPosition([[1, 0.0], [2, 0.0], [3, -90.0], [4, 0.0], [5, 0.0], [6, 0.0]], wait = True)
+    #Vertical Default Setback
+    arm.setPosition([[1, 0.0], [2, 0.0], [3, -90.0], [4, 0.0], [5, 0.0], [6, 0.0]], wait = True)
 
-#if __name__ == "main":
-    #hello_world()
+if __name__ == "main":
+    hello_world()
--- ui/ServosControlWidget.py
+++ ui/ServosControlWidget.py
@@ -25,9 +25,14 @@ class ServosControlWidget(QWidget):
             input_box = QLineEdit()
             input_box.setValidator(QDoubleValidator(-125.0, 125.0, 2))  # Allows floating point within range
             
+            # Set initial values for input boxes, with a special case for servo 3
+            initial_value = -90.0 if i == 2 else 0.0
+            input_box.setText(f"{initial_value}")
+            slider.setValue(int(initial_value * 4))
+            
             # Connect slider and input box to update each other
-            slider.valueChanged.connect(lambda value, lineEdit=input_box: lineEdit.setText(str(value / 4)))
-            input_box.textChanged.connect(lambda value, sldr=slider: sldr.setValue(int(float(value) * 4) if value else 0))
+            slider.valueChanged.connect(lambda value, i=i, lineEdit=input_box: self.sliderChanged(value, i, lineEdit))
+            input_box.textChanged.connect(lambda value, i=i, sldr=slider: self.lineEditChanged(value, i, sldr))
             
             servo_layout.addWidget(label)
             servo_layout.addWidget(slider)
@@ -38,6 +43,16 @@ class ServosControlWidget(QWidget):
         run_button = QPushButton('Run')
         run_button.clicked.connect(self.applyPositions)
         layout.addWidget(run_button)
+
+    def sliderChanged(self, value, servo_index, lineEdit):
+        # Prevent overwriting manual input if the focus is on the input box
+        if not lineEdit.hasFocus():
+            lineEdit.setText(str(value / 4))
+
+    def lineEditChanged(self, value, servo_index, slider):
+        # Only update slider if the change was not triggered by the slider itself
+        if not self.servo_controls[servo_index][0].isSliderDown():
+            slider.setValue(int(float(value) * 4) if value else 0)
 
     def applyPositions(self):
         for i, (slider, input_box) in enumerate(self.servo_controls):
