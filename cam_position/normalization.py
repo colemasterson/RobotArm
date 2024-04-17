31dfdc40e9ed68930f9d95a83ca596d1843dac77
Logan Gregg <Logan.Gregg@jacks.sdstate.edu>
2024-04-17 16:31:30 -0500

    Updated normalization changes

--- cam_position/normalization.py
+++ cam_position/normalization.py
@@ -15,6 +15,7 @@ import json
 
 def getPositionData():
      
+    data = []
     file_found = True   
     try:
         # Open the JSON file for reading
@@ -40,11 +41,10 @@ def getPositionData():
     except FileNotFoundError:
         print(f"No file found at '{file_path}'. Please check the file path.")
         file_found = False
-        return [], []  # Return empty lists in case of error
+
     except json.JSONDecodeError:
         print("Error decoding JSON. Please check the file content.")
         file_found = False
-        return [], []  # Return empty lists in case of error
     
     
     if (len(data) != 3 or file_found == False):
@@ -52,7 +52,7 @@ def getPositionData():
             [0.1561765, 0.08904364, 0.41930943],
             [0.15295053, 0.05556669, 0.41380159],
             [0.11283635, 0.02458074, 0.41529165]])
-        default_rvecs = np.array([(0.0, 0.0, 0.0), (0.0, 0.0, 0.0), (0.0, -90.0, 0.0)])
+        default_rvecs = np.array([(0.0, 0.0, 0.0), (0.0, 0.0, 0.0), (0.0, 0.0, 0.0)])
         
         return default_tvecs, default_rvecs
     
